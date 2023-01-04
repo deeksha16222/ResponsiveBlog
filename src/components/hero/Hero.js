87ca995eed904668be14ca5e1c06b73e760f1c08
Deeksha1998 <deekshabisht16@gmail.com>
2023-01-04 15:06:51 +0530

    third commit

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import HeroImage from "../../static/HeroImage.jpg"
+import HeroImage from "../../static/HeroImage.jpg";
 
 const Hero = () => {
-  return <Box>
-  <Typography align="center" variant="h3" sx={{fontWeight:900}}>
-  Fashions<b style={{color:"red"}}>Blog</b>
-  </Typography>
-  <Typography align="center" variant="body2" sx={{fontWeight:100}}>
-  We make you feel and look better!
-  </Typography>
-  <Box sx={{backgroundImage:`url(${HeroImage})`, backgroundRepeat:"no-repeat",backgroundColor:"black",backgroundPosition:"center",backgroundSize:"cover",height:600,width:"100%"}}></Box>
-  </Box>;
+  return (
+    <Box>
+      <Typography align="center" variant="h3" sx={{ fontWeight: 900 }}>
+        Fashions<b style={{ color: "red" }}>Blog</b>
+      </Typography>
+      <Typography align="center" variant="body2" sx={{ fontWeight: 100 }}>
+        We make you feel and look better!
+      </Typography>
+      <Box
+        sx={{
+          backgroundImage: `url(${HeroImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "black",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          height: 600,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{ width: {xs:"100%" ,sm:"50%",md:"40%"}, padding: {xs:3, sm:2, md:20} }}>
+          <Box sx={{ background: "white", opacity: "0.6" }}>
+            <Typography variant="h6" color="tomato" align="center" pt={8}>
+              Trending Styles
+            </Typography>
+            <Typography variant="h4" align="center">
+              Life is boring without fashion
+            </Typography>
+            <Typography variant="body1" align="center" pt={8}>
+              We love to change your Style!
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Hero;
